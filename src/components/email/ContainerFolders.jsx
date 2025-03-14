659b54e364e56f550cc709a5236a6a0ff34f693f
Philipp <skrall_pa@protonmail.com>
2025-03-14 08:45:05 +0100

    implement deleteFolder function

--- src/components/email/ContainerFolders.jsx
+++ src/components/email/ContainerFolders.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import ContextMenu from "./ContextMenu";
 
 const ContainerFolders = () => {
-    // Tracken der Änderungen am folders array sowie aktueller folder und ob folder geklickt wurde
     const [folders, setFolders] = useState([
         { id: -1, parent_id: null, folder_name: "root", clicked: false },
         { id: 0, parent_id: -1, folder_name: "Posteingang", clicked: false },
@@ -13,23 +12,15 @@ const ContainerFolders = () => {
     ]);
     const [curr_folder, setCurr_folder] = useState(folders[1]);
     const [context_menu, setContext_menu] = useState(null);
-    const [clicked, setClicked] = useState(false);
     const folder_emails = emails.filter(email => email.folder_id === curr_folder.id).sort((a, b) => b.datetime - a.datetime);
-    
     const folders_without_root = folders.filter(f => f.id !== -1);
+    
     curr_folder.clicked = true;
     const selectFolder = (folder) => {
-        // ToDo styling für geklickten Ordner implementieren
         curr_folder.clicked = false;
         setCurr_folder(folder);
         folder_emails.forEach(e => e.clicked = false);
     }
-    
-    const addFolder = (folder_name) => {
-        const folder_to_add = new Folder(-1, folder_name);
-        setFolders([...folders, folder_to_add]);
-        selectFolder(folder_to_add);
-    }    
 
     const getButtonClass = (folder) => {
         if (folder.clicked) {
@@ -49,16 +40,45 @@ const ContainerFolders = () => {
                 y: event.pageY,
                 type: "folder",
                 called_by: calling_folder, 
-                onDelete: () => handleDeleteFolder(calling_folder.id),
-                onAdd: (folder_name) => addFolder(folder_name),
+                onDelete: (to_delete_folder) => deleteFolder(to_delete_folder),
+                onAdd: (to_add_folder_name) => addFolder(to_add_folder_name),
                 handleClick: () => handleClick(),
             })
         }
     }
-    
-    const handleDeleteFolder = (folder_id) => {
-        console.log(`Lösche Ordner: ${folder_id}`);
-        // ToDo Implementieren
+    const addFolder = (to_add_folder_name) => {
+        const folder_to_add = new Folder(-1, to_add_folder_name);
+        setFolders([...folders, folder_to_add]);
+        selectFolder(folder_to_add);
+    }  
+
+    const deleteFolder = (to_delete_folder) => {
+        // Case: Papierkorb (Hier wird nicht der Ordner gelöscht, sondern nur der Papierkorb geleert)
+        // Dies ist die einzige Aktion bei der Mails wirklich gelöscht werden, sonst wird nur deren Ordner ID auf 2 gesetzt.
+        if (to_delete_folder.id === 2){
+            emails = emails.reduce((p, email) => (email.folder_id !== 2 && p.push(email),p),[]);
+            if (curr_folder.id === 2) setCurr_folder(folders[1]);
+        }
+        // Case: Parent ist root (Alle mails werden in Posteingang (PE) verschoben und alle child Ordner gelöscht (Mails auch in PE))
+        else if (to_delete_folder.parent_id === -1){
+            let child_folders = folders.filter(f => f.parent_id === to_delete_folder.id);
+            let fids_to_check = [to_delete_folder.id, ...child_folders.map(cf => cf.id)];
+            emails.forEach (e => {
+                if (fids_to_check.includes(e.folder_id)){
+                    e.folder_id = 0;
+                }
+            });
+            setFolders(folders.filter(f => !fids_to_check.includes(f.id)));
+        }
+        // Case: Else = Der Ordner ist ein Unterordner (Alle mails werden in Parentordner verschoben)
+        else {
+            emails.forEach (e => {
+                if (e.folder_id === to_delete_folder.id){
+                    e.folder_id = to_delete_folder.parent_id;
+                }
+            });
+            setFolders(folders.filter(f => f.id !== to_delete_folder.id));
+        }    
     };
 
     // Mapping der Folder in das Container Element
