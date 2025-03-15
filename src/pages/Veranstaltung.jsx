2fd12f0b12dfe75ebee277d056bb6f20e7dcbefb
mar0303 <scherfmarilis@gmail.com>
2025-03-15 22:36:06 +0100

    fix a mistake in Veranstaltung.jsx

--- src/pages/Veranstaltung.jsx
+++ src/pages/Veranstaltung.jsx
@@ -73,10 +73,10 @@ const Veranstaltung = () => {
           <Calendar className="w-70 text-[17px] text-center bg-indigo-50" />
           <br />
           {/*Unter dem Kalender wird die Aufgabenstellung der ausgewählten Aufgabe angezeigt*/}
-          <p className="text-[17px] border-2 border-uzk-dark p-1 rounded-xl sticky top-50 w-70">
+          <div className="text-[17px] border-2 border-uzk-dark p-1 rounded-xl sticky top-50 w-70">
             <p className="text-[20px] text-center font-bold">Ausgewählte Aufgabe: <br /></p>
             <p>{selectedAssignment || "Keine Aufgabe ausgewählt"} <br /></p>
-          </p>
+          </div>
         </div>
       </div>
       <Footer />
